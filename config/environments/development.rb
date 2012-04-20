8b784bcfbf37e38674c4fb488c77d9bda377c1ae
Attila Györffy <attila@attilagyorffy.com>
2012-04-20 17:52:40 +0100

    Make use of the remote debugging features when using Pow.
    You can now run the unobtrusive debugger with:
    `bundle exec rdebug -c`
    
    Make sure to have a ~/.powenv OR ~/.powrc file with the following in it:
    
    `export ENABLE_REMOTE_DEBUGGER_UNDER_POW=true`

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,3 +34,10 @@ Strano::Application.configure do
   
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
+
+if ENV['ENABLE_REMOTE_DEBUGGER_UNDER_POW'] && Debugger
+  Debugger.settings[:autoeval] = true
+  Debugger.settings[:autolist] = 1
+  Debugger.settings[:reload_source_on_change] = true
+  Debugger.start_remote
+end
